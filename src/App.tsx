4357f9d920d670724951b7bf24e47c089821a7d2
madhav <eragon.cube@outlook.com>
2023-11-21 20:33:42 +0530

    Update favicon and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import ComingSoon from './components/ComingSoon/ComingSoon.tsx';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header.tsx';
+import HomeScreen from './screens/HomeScreen.tsx';
+
+const comingSoon = true;
 
 function App() {
   return (
     <>
-      <ComingSoon />
+      {comingSoon ? (
+        <ComingSoon />
+      ) : (
+        <BrowserRouter basename='/enactus-main'>
+          <Header />
+          <Routes>
+            <Route index={true} path='/' element={<HomeScreen />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   );
 }
