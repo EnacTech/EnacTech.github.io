e21054d4e34bcfdc4304ccad460e1bcac48efb40
madhav <eragon.cube@outlook.com>
2023-11-21 09:35:36 +0530

    Add homepage and deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import ComingSoon from './components/ComingSoon';
+import ComingSoon from './components/ComingSoon/ComingSoon.tsx';
 import './App.css';
 
 function App() {
--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './ComingSoon.css';
 
 const ComingSoon = () => {
 
