420e6feae83bdb47588cbacee31108966e2b39cf
madhav <eragon.cube@outlook.com>
2023-12-11 17:15:07 +0530

    Completed Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,33 @@
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import Logo from '../assets/logo.png';
+import { NavLink } from 'react-router-dom';
 
-export default Header
+const Header = () => {
+  const navigation = [
+    { name: 'Home', path: '/' },
+    { name: 'About Us', path: '/about' },
+    { name: 'Projects', path: '/projects' },
+    { name: 'Team', path: '/team' },
+    { name: 'Contact Us', path: '/contact' },
+  ];
+  return (
+    <nav className='header'>
+      <div className='logo'>
+        <img src={Logo} alt='logo' />
+      </div>
+      <div className='nav-links'>
+        {navigation.map((item) => {
+          return (
+            <NavLink
+              to={item.path}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              {item.name}
+            </NavLink>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
